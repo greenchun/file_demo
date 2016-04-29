b725fe82b6b82fae3590414c75269f573cbe0432
Rick Wu <rick.wu@mitake.com.tw>
2016-04-29 17:36:43 +0800

    add something

--- app/src/main/java/com/example/givemepass/filedemo/MainActivity.java
+++ app/src/main/java/com/example/givemepass/filedemo/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.givemepass.filedemo;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private String data = "apk file";
     private String tmpData = "apk file tmp";
     private File fileTmp;
-    private Button internalWrite, internalRead, internalTmpWrite, internalTmpRead;
+    private Button internalWrite, internalRead, internalTmpWrite, internalTmpRead, extelnalPublic, extelnalPrivate;
     private TextView displayText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +32,10 @@ public class MainActivity extends AppCompatActivity {
         internalRead = (Button) findViewById(R.id.internal_read);
         internalTmpWrite = (Button) findViewById(R.id.internal_tmp_write);
         internalTmpRead = (Button) findViewById(R.id.internal_tmp_read);
+        extelnalPublic = (Button) findViewById(R.id.extelnal_public_folder);
+        extelnalPrivate = (Button) findViewById(R.id.extelnal_private_folder);
         displayText = (TextView) findViewById(R.id.display_text);
+
         internalWrite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,6 +60,27 @@ public class MainActivity extends AppCompatActivity {
                 internalTmpRead();
             }
         });
+        extelnalPublic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isExternalStorageWritable()){
+                    //可寫
+                    extelnalPublicCreateFoler();
+                } else if(isExternalStorageReadable()){
+                    //可讀
+                } else{
+                    //不可寫不可讀
+                }
+
+            }
+        });
+        extelnalPrivate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                extelnalPrivateCreateFoler();
+            }
+        });
+
     }
 
     //內部檔案寫入方式
@@ -111,5 +137,67 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    //外部空間建立公開資料夾
+    private void extelnalPublicCreateFoler(){
+        File dir = getExtermalStoragePublicDir("aa");
+        File f = new File(dir.getPath(), fileName);
+        try {
+            FileOutputStream outputStream = new FileOutputStream(f);
+            outputStream.write(data.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //外部空間建立私有資料夾
+    private void extelnalPrivateCreateFoler(){
+        File dir = getExtermalStoragePrivateDir("bb");
+        File f = new File(dir, fileName);
+        try {
+            FileOutputStream outputStream = new FileOutputStream(f);
+            outputStream.write(data.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private File getExtermalStoragePublicDir(String albumName) {
+        File file = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        if(file.mkdir()){
+            File f = new File(file, albumName);
+            if(f.mkdir()){
+                return f;
+            }
+        }
+        return new File(file, albumName);
+    }
+
+    private File getExtermalStoragePrivateDir(String albumName) {
+        File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+            Log.e("", "Directory not created or exist");
+        }
+        return file;
+    }
+
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
     }
 }
